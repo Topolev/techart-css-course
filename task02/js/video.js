c88655e238f2735231d0a09f7a61729303dc2bf0
Topolev <i.topolev.vladimir@gmail.com>
2017-07-25 14:53:59 +0300

    Task02: some refactorings

--- task02/js/video.js
+++ task02/js/video.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
 
     $(".video").each(function () {
 
@@ -9,7 +9,7 @@ $(document).ready(function () {
             var playButton = $(this).find(".video__play-pause");
             var mainPlayButton = $(this).find(".video__main-play");
 
-            var sound = $(this).find(".video__play-sound");
+            var sound = $(this).find(".video__sound");
             var seekProgress = $(this).find(".video__seek-progress");
 
             var seekBar = $(this).find(".video__seek-bar");
@@ -32,9 +32,20 @@ $(document).ready(function () {
             playButton.click(function () {
                 changeState();
             });
+
             mainPlayButton.click(function () {
                 changeState();
-            })
+            });
+
+            sound.click(function(){
+                video.muted = !video.muted;
+                sound.toggleClass("video__sound--no");
+            });
+
+            video.addEventListener("ended", function () {
+                console.log("fire-ended");
+                mainPlayButton.toggle("video__main-play--hide");
+            });
 
             if (seekBar.length != 0) {
                 video.addEventListener("timeupdate", function () {
@@ -50,10 +61,15 @@ $(document).ready(function () {
 
                 seekBar.change(function () {
                     // Calculate the new time
+
                     var time = video.duration * (seekBar.get(0).value / 100);
+                    console.log(seekBar.get(0).value, time);
+
 
                     // Update the video time
+                    console.log("cureentTIme", video.currentTime);
                     video.currentTime = time;
+                    console.log("cureentTIme", video.currentTime);
                 });
             }
 
